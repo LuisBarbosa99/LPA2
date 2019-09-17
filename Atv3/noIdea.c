2e799c7620ddf2f52f9cc030fa42d8524504c86b
Luis Barbosa <luis.barbosa@upe.br>
2019-09-17 18:21:26 -0300

    hex updated

--- Atv3/noIdea.c
+++ Atv3/noIdea.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int binario(int result){
+
+void binario(int result);  
+char conversao(char numero);
+void hex(int result);
+
+
+void binario(int result){
     
     int bin[8];
 
@@ -11,17 +17,64 @@ int binario(int result){
         bin[i]= result % 2;
         result= (result - bin[i]) / 2;
     }
-    
-    return bin;
+    for (int i = 0;i<8;i++){
+        printf("%i",bin[i]);
+    }
+    printf("\n");
 }
-int hex(){
-    int numero = 80,result;
+char conversao(char numero){
+    switch(numero){
+        case 10:
+            numero = 'A';
+            break;
+        case 11:
+            numero = 'B';
+            break; 
+        case 12:
+            numero = 'C';
+            break;
+        case 13:
+            numero = 'D';
+            break;
+        case 14:
+            numero = 'E';
+            break;
+        case 15:
+            numero = 'F';
+            break;
+    }
+
+    return numero;
+}
+void hex(int result){
     char hex[4];
-    
+    int index = 0;
+
+    while(result>=16){
+        hex[index] = result / 16;
+        hex[index+1] = result % 16;
+        result = hex[index];
+
+        if (hex[index] > 9){
+            hex[index] = conversao(hex[index]);
+        }
+        if (hex[index+1]>9){
+            hex[index+1] = conversao(hex[index+1]);
+        }
+        
+        index++;
+        
+    }
+    //printf("%s\n",&hex[0]);
+    for (int i = 0;i <= index;i++){
+        printf("%s",&hex[i]);
+    }
+    printf("\n");
 }
 int main()
 {
-    int result = 80;
-    printf("%i",binario(result));
+    int result = 254;
+    //binario(result);
+    hex(result);
     return 0;
 }
